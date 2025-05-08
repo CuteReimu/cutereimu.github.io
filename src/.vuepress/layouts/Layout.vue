107d7729443f65ec85128376d9299647a97ead63
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-05-08 11:56:18 +0800

    Bump deps and update pages

--- src/.vuepress/layouts/Layout.vue
+++ src/.vuepress/layouts/Layout.vue
@@ -1,15 +1,20 @@
 <template>
-  <div v-if="lastUpdated" class="update-time">
-    {{ lastUpdated.locale }}
-    <time :datetime="lastUpdated.iso.split(' ')[0]" data-allow-mismatch>
-      {{ lastUpdated.text.split(' ')[0] }}
-    </time>
-  </div>
+  <Layout>
+    <template #contentBefore>
+      <div v-if="lastUpdated" class="update-time">
+        {{ lastUpdated.locale }}
+        <time :datetime="lastUpdated.iso.split(' ')[0]" data-allow-mismatch>
+          {{ lastUpdated.text.split(' ')[0] }}
+        </time>
+      </div>
+    </template>
+  </Layout>
 </template>
 
 <script setup lang="ts">
 import {usePageFrontmatter} from "vuepress/client";
 import {useLastUpdated} from "@vuepress/plugin-git/client";
+import { Layout } from "vuepress-theme-hope/client";
 
 const frontmatter = usePageFrontmatter();
 const lastUpdated = useLastUpdated(() => frontmatter.value.lastUpdated as boolean ?? true);
