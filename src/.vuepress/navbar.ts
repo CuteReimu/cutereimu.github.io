24dd005c080b11450d8fd3041ccc4ecf6aec637b
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-05-12 16:26:08 +0800

    优化页面

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -3,10 +3,5 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/",
   "/programming/",
-  {
-    text: "日常随记",
-    icon: "pencil",
-    link: "/daily/hollowknight/",
-    prefix: "/daily/",
-  },
+  "/daily/",
 ]);
