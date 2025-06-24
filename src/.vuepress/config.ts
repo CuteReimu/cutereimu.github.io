c65357a174f342c8b4f38fbb73ce71102e29e837
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-06-24 19:54:44 +0800

    优化页面

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,7 +7,7 @@ export default defineUserConfig({
 
   lang: "zh-CN",
   title: "奇葩的灵梦的个人空间",
-  description: "超10年经验的资深服务端工程师，Go / Java / Kotlin / Erlang / C++ / Python / TypeScript / Vue 苦手",
+  description: "超10年经验的资深游戏服务端工程师，Go / Java / Kotlin / Erlang / C++ / Python / TypeScript / Vue 苦手",
 
   theme,
 });
