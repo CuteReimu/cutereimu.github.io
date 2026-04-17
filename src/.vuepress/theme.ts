1f708465f38aa0a935f68de2d28d13f96ab80060
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-04-17 17:56:59 +0800

    优化灵丝与灵魂前置页面

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -65,6 +65,7 @@ export default hopeTheme({
   copyright: '© 2025 奇葩的灵梦 | 基于 <a href="https://creativecommons.org/licenses/by-sa/4.0/" rel="noopener noreferrer" target="_blank">CC BY-SA 4.0</a> 共享',
 
   markdown: {
+    layout: true,
     chartjs: true,
 
     sup: true,
