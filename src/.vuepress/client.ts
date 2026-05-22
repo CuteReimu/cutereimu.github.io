f7e08eb5353d0b23caa28a44d96e0df32b11ee4a
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-05-22 19:39:14 +0800

    修复编译错误

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,6 +2,7 @@ import { defineClientConfig } from "vuepress/client";
 import "vuepress-theme-hope/presets/bounce-icon.scss";
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
+import { ID_INJECTION_KEY, ZINDEX_INJECTION_KEY } from 'element-plus';
 
 function syncElDarkClass() {
   const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
@@ -9,6 +10,10 @@ function syncElDarkClass() {
 }
 
 export default defineClientConfig({
+  enhance({ app }) {
+    app.provide(ID_INJECTION_KEY, { prefix: Math.floor(Math.random() * 100000000), current: 0 });
+    app.provide(ZINDEX_INJECTION_KEY, { current: 0 });
+  },
   setup() {
     if (typeof window !== 'undefined') {
       syncElDarkClass();
