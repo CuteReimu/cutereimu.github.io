6c7e95dbe08bc89a1c108e42a070a3227deae375
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-06-25 11:11:07 +0800

    增加slimsearch

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -134,7 +134,10 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     blog: true,
-    slimsearch: true,
+    slimsearch: {
+      suggestion: false,
+      indexContent: false,
+    },
 
     icon: {
       assets: "/@fortawesome/fontawesome-free@6/js/all.min.js",
